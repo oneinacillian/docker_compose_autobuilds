6038cf22cebb94acbaa76b860604cb6fc2a21e97
vsgideon <gideon.van.schalkwyk@gmail.com>
2024-02-13 21:58:30 +0100

    incorporate checks on hyperion and atomic services

--- atomic/Deployment/entrypoint.sh
+++ atomic/Deployment/entrypoint.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 set -e
 
-wait_for_service() {
-    echo "Checking availability of $1:$2"
-    timeout 60 bash -c "until echo > /dev/tcp/$1/$2; do sleep 1; done"
-    if [ $? -ne 0 ]; then
-        echo "Service $1:$2 did not become available"
-        exit 1
-    fi
-    echo "$1:$2 is available"
-}
-
-wait_for_service redis 6379
-wait_for_service postgres 5432
-
 # Navigate to the working directory
 cd /app/eosio-contract-api
 
 # Check if START_ON_LAUNCH is true and execute the pm2 command
 if [ "$ATOMIC_LAUNCH_ON_STARTUP" = "true" ]; then
+
+    wait_for_service() {
+        echo "Checking availability of $1:$2"
+        timeout 60 bash -c "until echo > /dev/tcp/$1/$2; do sleep 1; done"
+        if [ $? -ne 0 ]; then
+            echo "Service $1:$2 did not become available"
+            exit 1
+        fi
+        echo "$1:$2 is available"
+    }
+
+    wait_for_service redis 6379
+    wait_for_service postgres 5432
+
     echo "Starting pm2 with eosio-contract-api-filler..."
     pm2 start ecosystems.config.json --only eosio-contract-api-filler
     # Keep pm2 running in the foreground to prevent the container from exiting
--- hyperion/Deployment/entrypoint.sh
+++ hyperion/Deployment/entrypoint.sh
@@ -3,6 +3,27 @@ set -e
 
 # Check if LAUNCH_ON_STARTUP is true and execute start.sh
 if [ "$HYPERION_LAUNCH_ON_STARTUP" = "true" ]; then
+
+    wait_for_service() {
+        host="$1"
+        port="$2"
+        echo "Waiting for ${host}:${port}..."
+        while ! timeout 1 bash -c "cat < /dev/null > /dev/tcp/${host}/${port}"; do   
+            echo "Waiting for ${host}:${port}..."
+            sleep 2
+        done
+        echo "${host}:${port} is available."
+    }
+
+    # Wait for RabbitMQ, Elasticsearch, and Redis
+    wait_for_service rabbitmq 5672
+    wait_for_service es1 9200
+    wait_for_service es2 9200
+    wait_for_service es3 9200
+    wait_for_service redis 6379
+    wait_for_service node 8888
+    wait_for_service node 9876
+
     echo "Launching start.sh on startup..."
     bash run.sh wax-indexer
     # tail -f /apps/wax/stderr.txt
